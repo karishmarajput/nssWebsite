4e205e2f26d1ffa0ea6d96226b3580f99d2fff76
Pratik <pratikshrivastav20@gmail.com>
2023-06-01 13:52:35 +0530

    event and volunteer

--- DB/connection.js
+++ DB/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const URI = "mongodb://localhost:27017/nssDb";
+const URI = "mongodb+srv://fcritnss:u7J78YVINhBck4sR@cluster0.v0voyza.mongodb.net/";
 
 const connectDB = async () => {
   await mongoose.connect(URI, {
